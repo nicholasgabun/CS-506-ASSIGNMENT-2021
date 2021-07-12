374cf4945555f9629baa2597fd447b39d65d66bc
RUTEX92 <enoch.sylvanus@biosec.com.ng>
2021-07-12 12:16:30 +0100

    Added the updated sql file

--- cshop.sql
+++ cshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 29, 2021 at 09:52 AM
+-- Generation Time: Jul 12, 2021 at 01:14 PM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.4.8
 
@@ -24,6 +24,48 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `comment_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `comment_body` text NOT NULL,
+  `commented_on` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`comment_id`, `post_id`, `user_id`, `comment_body`, `commented_on`) VALUES
+(1, 1, 2, 'Yea I love the products too the last PC I bought was quite amazing, good battery life, etc', '2021-07-12 09:02:21');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `post_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `post_message` text NOT NULL,
+  `posted_on` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`post_id`, `user_id`, `post_message`, `posted_on`) VALUES
+(1, 1, 'Wow nice one C-Shop management I love your products', '2021-07-12 06:52:14'),
+(2, 2, 'Laptops are quite expensive now. How can I get cheaper ones people.', '2021-07-12 11:24:22');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -33,12 +75,33 @@ CREATE TABLE `users` (
   `email` varchar(30) NOT NULL,
   `phone` varchar(20) NOT NULL,
   `address` varchar(60) NOT NULL,
-  `password` varchar(200) NOT NULL
+  `password` varchar(200) NOT NULL,
+  `photo` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `phone`, `address`, `password`, `photo`) VALUES
+(1, 'Adamu Bashir', 'test@gmail.com', '', '', '$2y$10$ol24WmGFZ8FWsQu/cdAcM.9Ykos1s.eCwFCJra.n.1/.AMouFuQei', 'uploads/profile_pics/60ec19ccbb5a3.jpg'),
+(2, 'Ruth Sanda', 'rutex@gmail.com', '07032298254', '1 Prof. Ikejiani Clark Street', '$2y$10$R0W2r6Qbp0AqfRdArOgUEecep2OhRYkH8bkizSpv8MDJP5pBq6ApO', 'uploads/profile_pics/60ec19964f355.jpg');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`comment_id`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`post_id`);
 
 --
 -- Indexes for table `users`
@@ -52,10 +115,22 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
